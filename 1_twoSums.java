1380d8fcd23ac505b2fd2574d313ab86e127ebe0
UparSeEngineer <uparseengineer@gmail.com>
2025-08-28 11:50:10 +0530

    problem updated

--- 1_twoSums.java
+++ 1_twoSums.java
@@ -1,14 +1,11 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        int[] ans = new int[2];
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if (nums[i]+nums[j]==target){
-                    return new int []{i,j};
-                }
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (nums[j] + nums[j - i] == target)
+                return new int[] {j, j - i};
             }
         }
-        return ans;
+        return null;
     }
 }
